2f9ddd25f0696001012a38d760560290544641d9
Abishek Babu <abishekbabu000@gmail.com>
2018-09-23 01:31:22 +0530

    End stage

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const rate = document.querySelector("#rate");
 const rateValue = document.querySelector("#rate-value");
 const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
+const body = document.querySelector("body");
 
 //Init Voice Array
 let voices = [];
@@ -43,12 +44,17 @@ const speak = () => {
     return;
   }
   if (textInput.value !== "") {
+    //Add Bg Anime
+    body.style.background = "#141414 url(../img/wave.gif)";
+    body.style.backgroundRepeat = "repeat-x";
+    body.style.backgroundSize = "100% 100%";
     // Get speak text
     const speakText = new SpeechSynthesisUtterance(textInput.value);
 
     //speak end
     speakText.onend = e => {
       console.log("Done speaking");
+      body.style.background = "#141414";
     };
 
     //speak error
