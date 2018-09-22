f04413cc549601e01f014e4933cf9ad063c515e3
Abishek Babu <abishekbabu000@gmail.com>
2018-09-23 00:50:29 +0530

    Added voices and language data from API

--- js/main.js
+++ js/main.js
@@ -11,3 +11,26 @@ const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
 
 //Init Voice Array
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+
+  //loop through voices and create option for each
+  voices.forEach(voice => {
+    //create option element
+    const option = document.createElement("option");
+    //Fill the option with voice
+    option.textContent = voice.name + "(" + voice.lang + ")";
+
+    //set needed option attributes
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+  });
+};
+
+getVoices();
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
