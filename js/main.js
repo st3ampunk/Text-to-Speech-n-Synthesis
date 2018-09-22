25a505748b0da2be33cf84ef01c94196e64d7996
Abishek Babu <abishekbabu000@gmail.com>
2018-09-23 00:37:11 +0530

    Init SpeechSynth API

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//init speechSynth API
+const synth = window.speechSynthesis;
+
+// DOM elements
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+
+//Init Voice Array
